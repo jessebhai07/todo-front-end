7b3dbf3e5dca32caa74278022dfe35583f91547c
jessebhai07 <jessebhai189@gmail.com>
2025-07-13 01:12:54 +0800

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:4500/api', // Change if your backend port is different
+    baseURL: 'https://todo-backend-uta5.vercel.app/api', // Change if your backend port is different
 });
 
 export default API;
